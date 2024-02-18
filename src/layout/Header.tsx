7a4122843554022dc5b6dce68308d50f198c8b02
mbalyi <balyi.mark@hotmail.com>
2024-02-18 12:41:41 +0100

    image size optimisation

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
-import HeaderImg from "./../assets/header.jpeg";
+import SmallImg from "./../assets/header-small.jpg";
+import MediumImg from "./../assets/header-medium.jpg";
+import LargeImg from "./../assets/header-large.jpg";
 
 export const Header = () => (
   <Box
@@ -8,6 +10,8 @@ export const Header = () => (
       width: "100%",
     }}
     alt="Cat breed header image"
-    src={HeaderImg}
+    src={LargeImg}
+    srcSet={`${SmallImg} 400w, ${MediumImg} 800w, ${LargeImg} 1200w`}
+    sizes="(max-width: 600px) 100vw, (max-width: 1200px) 100vw, 100%"
   />
 );
